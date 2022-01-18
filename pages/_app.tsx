c8c7e0eaa02b612a325951cbf1ad01f5e9e38e55
aln1234 <albinlamichhane9@gmail.com>
2022-01-18 17:16:40 +0545

    changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,38 +2,34 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from "../components/Layout"
 import Script from 'next/script'
+import * as gtag from '../lib/gtag'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 
-{/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-DYWT468JBV"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-DYWT468JBV');
-</script> */}
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
-    <Script
-    strategy='lazyOnload'
-    src={"https://www.googletagmanager.com/gtag/js?id=G-DYWT468JBV"}
-    />
-      <Script
-    strategy='lazyOnload'
-   
-    >
-      {
-      ` window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-      
-        gtag('config', 'G-DYWT468JBV');"
-        `
-      }
-
-    </Script>
+ 
     <Layout>
        <Component {...pageProps} />
 
